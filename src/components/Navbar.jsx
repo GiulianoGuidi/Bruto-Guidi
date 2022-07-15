7d4af7595e9287d251ae0ea1bf7509f50aeb48de
Giuliano <giulioguidi10@gmail.com>
2022-07-15 19:39:11 -0300

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar_menu'>
+
+        <button className='Navbar_button'>Menú</button>
+        <button className='Navbar_button'>Productos</button>
+        <button className='Navbar_button'>Sobre Nosotros</button>
+        <button className='Navbar_button'>Contacto</button>
+    
+    </div>
+  )
+} 
+
+export default Navbar
